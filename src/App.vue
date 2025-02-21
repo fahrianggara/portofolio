81dc7c2149b6632169ea71db31c4b1542276962a
Fahri Anggara <fahriangga30@gmail.com>
2025-02-21 12:51:40 +0700

    feat: add Footer component and implement dark mode functionality in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <script setup>
-  import Navbar from './components/Navbar.vue'
+  import Navbar from './components/Navbar.vue';
+  import Footer from './components/Footer.vue';
+  import { useRoute } from 'vue-router';
+  import { onMounted } from 'vue';
+  import { useDark, useToggle } from '@vueuse/core';
+
+  const route = useRoute();
+
+  const isDark = useDark({
+    selector: 'html',
+    attribute: 'data-theme',
+    valueDark: 'dark',
+    valueLight: 'light',
+  });
+  const toggleDark = useToggle(isDark);
+
+  onMounted(() => { // Add dark:bg-black class to body element
+    document.body.classList.add('dark:bg-black');
+  });
 </script>
 
 <template>
-  <Navbar :class="'font-inter'" />
+  <Navbar v-if="route.meta.showNavbarAndFooter" :class="'font-inter'" :isDark="isDark" :toggleDark="toggleDark" />
   <router-view :class="'font-inter'" />
+  <Footer v-if="route.meta.showNavbarAndFooter" :class="'font-inter'" />
 </template>
 
 <style scoped>
