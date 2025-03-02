8cbb5251b821fe822654d16b432d259e5347c794
Fahri Anggara <fahriangga30@gmail.com>
2025-03-02 11:53:15 +0700

    feat: enhance ProjectsView with new content structure, add Pagination and Repositories components, and update styles

---
+++ src/components/projects/Repositories.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="text-lg font-medium mb-4 mt-6">My Repositories</h1>
+  <ol class="repos">
+    <li v-for="repo in [1, 2, 3, 4]" :key="repo">
+      <a href="#" class="">
+        <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
+          <path d="M19,3H12.472a1.019,1.019,0,0,1-.447-.1L8.869,1.316A3.014,3.014,0,0,0,7.528,1H5A5.006,5.006,0,0,0,0,6V18a5.006,5.006,0,0,0,5,5H19a5.006,5.006,0,0,0,5-5V8A5.006,5.006,0,0,0,19,3ZM5,3H7.528a1.019,1.019,0,0,1,.447.1l3.156,1.579A3.014,3.014,0,0,0,12.472,5H19a3,3,0,0,1,2.779,1.882L2,6.994V6A3,3,0,0,1,5,3ZM19,21H5a3,3,0,0,1-3-3V8.994l20-.113V18A3,3,0,0,1,19,21Z"/>
+        </svg>
+
+        <div class="content">
+          <h3 class="text-[16px] font-semibold mb-1">Repository Name</h3>
+          <p class="text-base/relaxed text-[15px] text-gray-600 dark:text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.
+          </p>
+        </div>
+      </a>
+    </li>
+  </ol>
+</template>
+
+<style scoped>
+  @reference 'tailwindcss';
+  @import '@/assets/main.css';
+
+  .repos {
+    @apply grid sm:grid-cols-2 grid-cols-1 gap-4;
+  }
+
+  .repos li {
+    @apply relative p-4 rounded-2xl bg-white/40 dark:bg-dark-surface/60 backdrop-blur-2xl border border-solid dark:border-zinc-900 border-gray-300 ease-in-out duration-300 transform hover:-translate-y-[2px]
+    dark:hover:bg-dark-surface/80 hover:bg-white/70 ;
+  }
+
+  .repos svg {
+    @apply w-6 h-6 fill-primary mb-3;
+  }
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,7 +1,8 @@
 <script setup>
   import Sidebar from "@/components/Sidebar.vue";
   import { useScreenSize } from "@/utils/screenResize.js";
-
+  import Repositories from "@/components/projects/Repositories.vue";
+  import Pagination from "@/components/projects/Pagination.vue";
   const { resizeScreen } = useScreenSize();
 </script>
 
@@ -9,29 +10,36 @@
   <section>
     <div class="container">
       <div class="grid grid-cols-1 md:grid-cols-3 gap-0 md:gap-6 lg:gap-8 relative">
-        <div class="dark:text-white mb-3 hidden md:block relative" v-if="!resizeScreen">
+        <div class="dark:text-white hidden md:block relative" v-if="!resizeScreen">
           <Sidebar :class="'sticky top-26'" />
         </div>
-        <div class="dark:text-white col-span-2">
-          <h1 class="text-2xl mb-3">Ini halaman PROJECTS</h1>
-          <p class="mb-3">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur velit facere corporis, voluptas omnis ut, laudantium repellendus tempora explicabo in dolorum labore culpa, est esse molestiae quia voluptates vero cupiditate.
-          </p>
-          <p class="mb-3">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur velit facere corporis, voluptas omnis ut, laudantium repellendus tempora explicabo in dolorum labore culpa, est esse molestiae quia voluptates vero cupiditate.
-          </p>
-          <p class="mb-3">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur velit facere corporis, voluptas omnis ut, laudantium repellendus tempora explicabo in dolorum labore culpa, est esse molestiae quia voluptates vero cupiditate.
-          </p>
-          <p class="mb-3">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur velit facere corporis, voluptas omnis ut, laudantium repellendus tempora explicabo in dolorum labore culpa, est esse molestiae quia voluptates vero cupiditate.
-          </p>
-          <p class="mb-3">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur velit facere corporis, voluptas omnis ut, laudantium repellendus tempora explicabo in dolorum labore culpa, est esse molestiae quia voluptates vero cupiditate.
-          </p>
-          <p class="mb-3">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur velit facere corporis, voluptas omnis ut, laudantium repellendus tempora explicabo in dolorum labore culpa, est esse molestiae quia voluptates vero cupiditate.
-          </p>
+        <div class="dark:text-white col-span-2 mb-5">
+          <h1 class="text-lg font-medium mb-4">My Projects</h1>
+          <ol class="projects">
+            <li v-for="project in [1, 2, 3]" :key="project">
+              <router-link to="/projects/1" class="flex items-start gap-4">
+                <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" class="" />
+                <div class="w-full h-full flex flex-col justify-between">
+                  <div>
+                    <span class="text-[12px] sm:text-[13px] dark:text-gray-600 text-gray-400 font-medium mb-1 block">
+                      UI/UX Design
+                    </span>
+                    <h3 class="text-[15px] sm:text-[16px] md:text-lg font-semibold mb-1 
+                      line-clamp-1 duration-300 ease-in-out">
+                      Sistem Aplikasi Penjualan Online Berbasis Web aaaaaaaa
+                    </h3>
+                  </div>
+                  <p class="text-base/relaxed text-[13px] sm:text-[15px] text-gray-600 dark:text-gray-400 
+                    line-clamp-2 mt-auto">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit enim blanditiis, eum vel culpa, facere suscipit aliquid quae quos molestiae quod, nostrum quisquam soluta. Laudantium porro in aspernatur aliquid enim.
+                  </p>
+                </div>
+              </router-link>
+            </li>
+          </ol>
+          <Pagination />
+
+          <Repositories />
         </div>
       </div>
     </div>
@@ -39,5 +47,22 @@
 </template>
 
 <style scoped>
+  @reference 'tailwindcss';
+  @import '@/assets/main.css';
 
+  .projects {
+    @apply flex flex-col gap-4;
+  }
+
+  .projects li a  {
+    @apply relative p-4 rounded-2xl bg-white/40 dark:bg-dark-surface/60 backdrop-blur-2xl border border-solid dark:border-zinc-900 border-gray-300 ease-in-out duration-300 transform hover:-translate-y-[2px]
+    dark:hover:bg-dark-surface/80 hover:bg-white/70;
+  }
+  .projects li a img {
+    @apply w-20 h-20 md:w-28 md:h-28 object-cover rounded-xl;
+  }
+  
+  .projects li a:hover h3 {
+    @apply text-primary;
+  }
 </style>
