1e4a7494653c154f1e0416c72c1759f7796d6f39
Fahri Anggara <fahriangga30@gmail.com>
2025-03-04 10:38:59 +0700

    feat: standardize grid layout across multiple views by removing redundant lg:gap-8 class

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -8,7 +8,7 @@
 <template>
   <section>
     <div class="container">
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-0 md:gap-6 lg:gap-8 relative">
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-0 md:gap-6 relative">
         <div class="dark:text-white mb-3 hidden md:block relative" v-if="!resizeScreen">
           <Sidebar :class="'sticky top-26'" />
         </div>
