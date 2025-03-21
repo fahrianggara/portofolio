7451a250f7ffa45becc903ec639b70d1957fb95d
Fahri Anggara <fahriangga30@gmail.com>
2025-03-21 13:53:09 +0700

    chore: remove unused files and configurations from the project

--- src/components/icon/Contrast.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" x="0" y="0" viewBox="0 0 24 24" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0Zm0 22V2c5.51 0 10 4.49 10 10s-4.49 10-10 10Z" ></path></g></svg>
-</template>
